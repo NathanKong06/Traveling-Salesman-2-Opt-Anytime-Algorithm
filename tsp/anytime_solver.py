21555befa632db78582394586606bba8efef5307
NathanKong06 <81611522+NathanKong06@users.noreply.github.com>
2025-12-05 22:05:38 -0800

    Add restarts after hitting maxima

--- tsp/anytime_solver.py
+++ tsp/anytime_solver.py
@@ -1,20 +1,25 @@
 from tsp.tour import create_random_tour
 from tsp.geometry import tour_length
 from tsp.two_opt import try_two_opt
-import time
 
 def anytime_tsp(points):
     tour = create_random_tour(len(points))
     length = tour_length(tour, points)
-    yield tour, length
+
+    best_tour = tour[:]
+    best_length = length
+    yield best_tour, best_length
 
     while True:
         result = try_two_opt(tour, points)
 
         if result is None:
-            yield tour, length
-            time.sleep(0.1)
-            continue
+            tour = create_random_tour(len(points))
+            length = tour_length(tour, points)
+        else:
+            tour, length = result
 
-        tour, length = result
-        yield tour, length
+        if length < best_length:
+            best_tour = tour[:]
+            best_length = length
+            yield best_tour, best_length
